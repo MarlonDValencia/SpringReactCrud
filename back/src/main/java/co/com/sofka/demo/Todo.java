22f0639b6f7fe40c2353b527a5bedbdde9c076ab
MarlonDValencia <marlondvalencia@gmail.com>
2021-11-26 14:35:44 -0500

    React.js setted up

--- back/src/main/java/co/com/sofka/demo/Todo.java
+++ back/src/main/java/co/com/sofka/demo/Todo.java
@@ -1,2 +1,40 @@
-package co.com.sofka.demo;public class Todo {
+package co.com.sofka.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private boolean idCompletado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isIdCompletado() {
+        return idCompletado;
+    }
+
+    public void setIdCompletado(boolean idCompletado) {
+        this.idCompletado = idCompletado;
+    }
 }
--- back/src/main/java/co/com/sofka/demo/TodoController.java
+++ back/src/main/java/co/com/sofka/demo/TodoController.java
@@ -1,2 +1,37 @@
-package co.com.sofka.demo;public class TodoController {
+package co.com.sofka.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+    @GetMapping(value = "api/todos")
+    public Iterable<Todo> list(){
+        return service.list();
+    }
+    @PostMapping(value = "api/{id}/todo")
+    public Todo save(@RequestBody Todo todo){
+        return service.save(todo);
+    }
+    @PutMapping(value = "api/{id}/todo")
+    public Todo update(@RequestBody Todo todo) {
+        if (todo.getId() != null) {
+            return service.save(todo);
+        } else {
+            throw new RuntimeException("No existe el id para actualizar");
+        }
+    }
+    @DeleteMapping(value ="api/{id}/todo")
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
+    @GetMapping(value = "api/{id}/todo")
+    public Todo get(@PathVariable("id") Long id){
+        return service.get(id);
+    }
+
 }
